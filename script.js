69026f488e7c94508d741d32d703d1edd1da0e52
RegulusBlack79 <isabela.bloch@escola.pr.gov.br>
2025-10-27 09:20:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(buttom => {
+    buttom.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+    })
+})
